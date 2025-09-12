993a9a03f9f3162171ebf45a8b0386cfd678764a
Francis Stephens <francis.stephens@gmail.com>
2025-09-12 21:02:23 +1200

    Allow change broadcaster to be de/reactivated
    There is a bug caused by having the field updates caused by data-changed
    events generating their own data-changed events which are then further
    broadcast. So far we have tried several approaches to resolve this
    problem.
    
    1: Set an id on broadcast data-changed events. Don't consume your own
       data-changed events. This prevents a data-change infinite loop, but
    other problems remain.
    2: Only allow an input/output to be updated if the data being changed
       actually changes the value stored in the field.
    
    There was a new bug which caused a kind of unbounded update loop which
    would grow the value being set at each iteration. This would eventually
    cause a panic when the value being updated was too large for the bitsize
    of the field being set.
    
    The actual cause was never fully understood.
    
    However, simply preventing all field updates which happen in response to
    a data-changed event broadcast resolves all of these problems. We allow
    the RegisterInputs to deactivate the changeBroadcaster while it updates
    it's inputs in response to a data-changed event. Now no ping-pong
    update broadcasts are possible. This also dramatically simplifies the
    logs since events are only broadcast once.
    
    There may be a bug lurking in the code as the true cause of the growing
    update value is not known right now. Hopefully we will understand this
    in the future.

--- pkg/instructions/ui/change_broadcaster.go
+++ pkg/instructions/ui/change_broadcaster.go
@@ -8,11 +8,13 @@ type dataReceiver interface {
 }
 
 type changeBroadcaster struct {
+	active    bool
 	receivers []dataReceiver
 }
 
 func newChangeBroadcaster() *changeBroadcaster {
 	return &changeBroadcaster{
+		active:    true,
 		receivers: []dataReceiver{},
 	}
 }
@@ -22,11 +24,21 @@ func (b *changeBroadcaster) addReceiver(l dataReceiver) {
 }
 
 func (b *changeBroadcaster) broadcastChange(bytes []byte, senderId uuid.UUID) {
+	if !b.active {
+		return
+	}
+
 	for _, r := range b.receivers {
 		// Avoid updating the receiver that generated this change
 		// Updating the sender produces an infinite loop of update events
-		if r.receiverId() != senderId {
-			r.dstDataChanged(append(make([]byte, 0, len(bytes)), bytes...))
-		}
+		r.dstDataChanged(append(make([]byte, 0, len(bytes)), bytes...))
 	}
+}
+
+func (b *changeBroadcaster) activate() {
+	b.active = true
+}
+
+func (b *changeBroadcaster) deactivate() {
+	b.active = false
 }
