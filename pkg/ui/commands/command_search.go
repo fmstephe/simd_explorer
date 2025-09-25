911d591504330600bf8f04bd6fb95ead5f6c7ed6
Francis Stephens <francis.stephens@gmail.com>
2025-09-25 20:04:09 +1200

    Display assembly function in CommandSearch
    The bottom of the screen now displays the currently highlighted
    instructions assembly function. This feels quite nice, I wonder how
    useful people will find it?

--- pkg/ui/commands/command_search.go
+++ pkg/ui/commands/command_search.go
@@ -12,20 +12,42 @@ import (
 type CommandSearch struct {
 	list  *tview.List
 	input *tview.InputField
-	grid  *tview.Grid
+	flex  *tview.Flex
 }
 
 func NewCommandSearch(instructions []assembly.Instruction, app *stackapp.StackApp) *CommandSearch {
 	instMap, instNames := buildInstructionMap(instructions)
+
+	assemblyView := tview.NewTextView()
+	assemblyView.SetBorder(true)
+	assemblyView.SetTitle("Go Assembly")
+
 	list := tview.NewList()
 	list.SetBorder(true)
 	list.SetTitle("Choose An Instruction")
 	list.ShowSecondaryText(true)
-	for name, inst := range instMap {
+
+	// Build list of instructions
+	for _, name := range instNames {
+		inst := instMap[name]
 		list.AddItem(name, inst.Description(), 0, func() {
-			rs := register.NewUIRegisterSet(app, inst)
-			app.Push(rs.Base16.GetPrimitive())
+			if inst.Supported() {
+				// If the instruction is supported, display the ui for it
+				rs := register.NewUIRegisterSet(app, inst)
+				app.Push(rs.Base16.GetPrimitive())
+			}
 		})
+	}
+
+	// When a new list item is selected, update the assembly view to
+	// display the assembly for that instruction
+	list.SetChangedFunc(func(index int, mainText string, _ string, _ rune) {
+		assemblyView.SetText(instMap[mainText].Assembly())
+	})
+
+	// Set the assembly view from the first instruction in the list
+	if len(instNames) > 0 {
+		assemblyView.SetText(instMap[instNames[0]].Assembly())
 	}
 
 	input := tview.NewInputField()
@@ -34,12 +56,27 @@ func NewCommandSearch(instructions []assembly.Instruction, app *stackapp.StackAp
 
 	input.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Key() {
-		// TAB or SHIFT+TAB cycles focus to the list
-		case tcell.KeyTAB, tcell.KeyBacktab:
-			app.SetFocus(list)
 		case tcell.KeyEnter:
 			listIdx := list.GetCurrentItem()
 			list.GetItemSelectedFunc(listIdx)()
+			// Short-circuit default handling of the enter key
+			return nil
+		case tcell.KeyUp, tcell.KeyBacktab:
+			listIdx := list.GetCurrentItem()
+			list.SetCurrentItem(listIdx - 1)
+			// Short-circuit default handling of the up/shift+tab keys
+			return nil
+		case tcell.KeyDown, tcell.KeyTAB:
+			listIdx := list.GetCurrentItem()
+			list.SetCurrentItem(listIdx + 1)
+			// Short-circuit default handling of the down/tab keys
+			return nil
+		case tcell.KeyLeft, tcell.KeyRight:
+			// Disable left/right text input naviation
+			// Users are likely looking at the instruction list
+			// while typing, so only very simple input/delete
+			// interactions are supported
+			return nil
 		}
 		return event
 	})
@@ -55,40 +92,21 @@ func NewCommandSearch(instructions []assembly.Instruction, app *stackapp.StackAp
 		}
 	})
 
-	// Text input directed to the list is appended to the inputfield This
-	// allows text input to continue to work even if the list itself has
-	// been tabbed to or clicked on
-	list.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		switch event.Key() {
-		// Deletions cause a deletion from the input-field
-		case tcell.KeyDelete, tcell.KeyDEL:
-			input.SetText(deleteFrom(input.GetText()))
-		// Text input is appended to the input-field
-		case tcell.KeyRune:
-			input.SetText(input.GetText() + string(event.Rune()))
-		// TAB or SHIFT+TAB cycles focus to the input-field
-		case tcell.KeyTAB, tcell.KeyBacktab:
-			app.SetFocus(input)
-			// We interrupt the event processing here, otherwise
-			// the list will interpret TAB as navigating the list
-			return nil
-		}
+	flex := tview.NewFlex()
+	flex.SetDirection(tview.FlexRow)
 
-		return event
-	})
-
-	grid := tview.NewGrid()
-	grid.AddItem(list, 0, 0, 1, 1, 0, 0, false)
-	grid.AddItem(input, 1, 0, 1, 1, 0, 0, true)
+	flex.AddItem(list, 0, 2, false)
+	flex.AddItem(input, 3, 1, true)
+	flex.AddItem(assemblyView, 0, 3, false)
 	return &CommandSearch{
 		list:  list,
 		input: input,
-		grid:  grid,
+		flex:  flex,
 	}
 }
 
-func (s *CommandSearch) GetBox() *tview.Grid {
-	return s.grid
+func (s *CommandSearch) GetBox() *tview.Flex {
+	return s.flex
 }
 
 func deleteFrom(txt string) string {
@@ -102,8 +120,12 @@ func buildInstructionMap(instructions []assembly.Instruction) (instMap map[strin
 	instMap = map[string]assembly.Instruction{}
 	instNames = []string{}
 	for _, inst := range instructions {
-		instMap[inst.Name()] = inst
-		instNames = append(instNames, inst.Name())
+		name := inst.Name()
+		if !inst.Supported() {
+			name = name + " (Instruction Not Supported!)"
+		}
+		instMap[name] = inst
+		instNames = append(instNames, name)
 	}
 
 	return instMap, instNames
