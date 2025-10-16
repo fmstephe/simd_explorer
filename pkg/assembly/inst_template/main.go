d702ac5ce929846e7b0d1c137e3030fe8c9ac2cc
Francis Stephens <francis.stephens@gmail.com>
2025-10-16 21:21:52 +1300

    First draft of templating for new instructions

---
+++ pkg/assembly/inst_template/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"text/template"
+)
+
+var (
+	flagInstruction       = flag.String("instruction", "", "The assembly name of the instruction to be demonstrated")
+	flagSizeClass         = flag.Int("size-class", -1, "The size class of the instruction being demonstrated. Many SIMD instructions work across a range of register sizes.")
+	flagDiscriminator     = flag.String("discriminator", "", "A discriminator (can be empty) useful when to demonstrate two versions of an instruction in the same size class, e.g. 'k' ")
+	flagTypeDiscriminator = flag.String("type-discriminator", "", "Temporary flag used to add discriminator to type. Will be removed later when unnecessary use of discriminators is removed.")
+)
+
+type templateValues struct {
+	// Basic Data
+	PackageName       string
+	InstructionUpper  string
+	SizeClass         int
+	Discriminator     string
+	TypeDiscriminator string
+
+	// Derived Data
+	FunctionName      string
+	FunctionNameTitle string
+	DemoTypeName      string
+
+	// File Names
+	AssemblyFileName          string
+	StubFileName              string
+	AssemblyGeneratorFileName string
+	DemoFileName              string
+}
+
+func main() {
+	flag.Parse()
+	validateFlags()
+
+	instructionLower := strings.ToLower(*flagInstruction)
+	instructionUpper := strings.ToUpper(*flagInstruction)
+	instructionTitle := strings.Title(instructionLower)
+	sizeClass := *flagSizeClass
+	discriminatorLower := strings.ToLower(*flagDiscriminator)
+	discriminatorTitle := strings.Title(discriminatorLower)
+	discriminatorUpper := strings.ToUpper(*flagDiscriminator)
+	// File names without discriminator unless needed
+	var fileNameSuffix string
+	if discriminatorLower != "" {
+		fileNameSuffix = fmt.Sprintf("%s_%d_%s", instructionLower, sizeClass, discriminatorLower)
+	} else {
+		fileNameSuffix = fmt.Sprintf("%s_%d", instructionLower, sizeClass)
+	}
+
+	tValues := &templateValues{
+		PackageName:               instructionLower,
+		InstructionUpper:          instructionUpper,
+		SizeClass:                 sizeClass,
+		Discriminator:             discriminatorLower,
+		DemoTypeName:              fmt.Sprintf("%s%d%s", instructionUpper, sizeClass, typeDiscriminator),
+		FunctionName:              fmt.Sprintf("%s%d%s", instructionLower, sizeClass, discriminatorTitle),
+		FunctionNameTitle:         fmt.Sprintf("%s%d%s", instructionTitle, sizeClass, discriminatorTitle),
+		AssemblyFileName:          fmt.Sprintf("asm_%d_%s_%s.s", sizeClass, discriminatorLower, instructionLower),
+		StubFileName:              fmt.Sprintf("stub_%d_%s_%s.go", sizeClass, discriminatorLower, instructionLower),
+		AssemblyGeneratorFileName: fmt.Sprintf("asm_%d_%s_%s.go", sizeClass, discriminatorLower, instructionLower),
+		DemoFileName:              fmt.Sprintf("%s_%d_%s.go", instructionLower, sizeClass, discriminatorLower),
+	}
+
+	fmt.Printf("%#v\n\n", tValues)
+
+	buildDirectories(tValues)
+	buildGenerator(tValues)
+	buildDemo(tValues)
+}
+
+func validateFlags() {
+	if *flagInstruction == "" {
+		fmt.Fprintf(os.Stderr, "Missing -instruction flag value\n")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	if *flagSizeClass == -1 {
+		fmt.Fprintf(os.Stderr, "Missing -size-class flag value\n")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	if *flagDiscriminator == "" {
+		fmt.Fprintf(os.Stderr, "Missing -description-short flag value\n")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+}
+
+func buildDirectories(tValues *templateValues) {
+	err := os.MkdirAll(tValues.PackageName+"/_generate", os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func buildGenerator(tValues *templateValues) {
+	fmt.Println("Generator", tValues)
+
+	f, err := os.Create(tValues.PackageName + "/_generate/" + tValues.AssemblyGeneratorFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	generatorTemplate, err := template.New("generator").Parse(asmTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	err = generatorTemplate.Execute(f, tValues)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func buildDemo(tValues *templateValues) {
+	fmt.Println("\n\nDemo", tValues)
+
+	f, err := os.Create(tValues.PackageName + "/" + tValues.DemoFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	instructionDemoTemplate, err := template.New("demo").Parse(instructionDemoSource)
+	if err != nil {
+		panic(err)
+	}
+
+	err = instructionDemoTemplate.Execute(f, tValues)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ pkg/assembly/inst_template/templates.go
@@ -0,0 +1,102 @@
+package main
+
+const asmTemplate = `package main
+
+import (
+	. "github.com/mmcloughlin/avo/build"
+	. "github.com/mmcloughlin/avo/operand"
+)
+
+//go:generate go run {{.AssemblyGeneratorFileName}} -out ../{{.AssemblyFileName}} -stubs ../{{.StubFileName}} -pkg {{.PackageName}}
+func main() {
+	// NOTE: This is a generic template that generates valid assembly for testing purposes.
+	// It does NOT implement the actual {{.InstructionUpper}} instruction.
+	// Replace this implementation with the correct {{.InstructionUpper}} instruction code.
+	TEXT("{{.FunctionName}}", NOSPLIT, "func(vals1, vals2, ret *[4]float32)")
+	Comment("load params")
+	vals1 := Load(Param("vals1"), GP64())
+	vals2 := Load(Param("vals2"), GP64())
+	ret := Load(Param("ret"), GP64())
+
+	Comment("Load vals1 into XMM register")
+	regX1 := XMM()
+	VMOVDQA(Mem{Base: vals1}, regX1)
+	
+	Comment("Load vals2 into XMM register")
+	regX2 := XMM()
+	VMOVDQA(Mem{Base: vals2}, regX2)
+
+	Comment("Sum packed float32 values from vals1 and vals2")
+	ADDPS(regX2, regX1)
+
+	Comment("Write results into return memory address")
+	VMOVDQA(regX1, Mem{Base: ret})
+
+	// generate!
+	Generate()
+}`
+
+const instructionDemoSource = `package {{.PackageName}}
+
+import (
+	_ "embed"
+	"log"
+
+	"github.com/fmstephe/simd_explorer/pkg/assembly/asmutil"
+	"github.com/fmstephe/simd_explorer/pkg/ui/number"
+)
+
+//go:embed {{.AssemblyFileName}}
+var assembly{{.FunctionNameTitle}} string
+
+//go:embed {{.StubFileName}}
+var stub{{.FunctionNameTitle}} string
+
+type {{.DemoTypeName}} struct {
+}
+
+func (v *{{.DemoTypeName}}) Inputs() []*number.Parameter {
+	// TODO
+	return nil
+}
+
+func (v *{{.DemoTypeName}}) Output() *number.Parameter {
+	// TODO
+	return nil
+}
+
+func (v *{{.DemoTypeName}}) Name() string {
+	return "{{.InstructionUpper}} ({{.SizeClass}} bit) {{.Discriminator}}"
+}
+
+func (v *{{.DemoTypeName}}) Description() string {
+	return "TODO"
+}
+
+func (v *{{.DemoTypeName}}) Stub() string {
+	return stub{{.FunctionNameTitle}}
+}
+
+func (v *{{.DemoTypeName}}) Assembly() string {
+	return assembly{{.FunctionNameTitle}}
+}
+
+func (v *{{.DemoTypeName}}) Run(inputs [][]byte) (output []byte) {
+	// Example arguments processing
+	floats1 := [8]float32{}
+	copy(floats1[:], number.ToFloat32Slice(inputs[0]))
+	floats2 := [8]float32{}
+	copy(floats2[:], number.ToFloat32Slice(inputs[1]))
+
+	ret := [8]float32{}
+
+	{{.FunctionName}}(/* TODO */)
+
+	log.Printf("{{.DemoTypeName}} input %v %v output %v", floats1, floats2, ret)
+
+	return number.Float32SliceToBytes(ret[:])
+}
+
+func (v *{{.DemoTypeName}}) Supported() bool {
+	return asmutil.IsSupported(v.Assembly())
+}`
