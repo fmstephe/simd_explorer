c0ea3b5c3a299fc5c1b86cc3d7a441b2259f192f
Francis Stephens <francis.stephens@gmail.com>
2026-01-21 21:29:47 +1300

    Write simdexplorer log to /tmp
    This is done is done in a platform agnostic manner. So it should work on
    Windows. Although it hasn't been tested there.

--- cmd/simdexplorer/main.go
+++ cmd/simdexplorer/main.go
@@ -3,15 +3,33 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/fmstephe/simd_explorer/pkg/ui"
 )
 
 func main() {
-	if f, err := os.OpenFile("./debug.log", os.O_TRUNC|os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666); err != nil {
-		log.Fatalf("Error opening log file %v", err)
-	} else {
-		log.SetOutput(f)
+	f, err := truncateAndOpenLog()
+	if err != nil {
+		log.Fatalf("Error preparing log file %v", err)
 	}
+	defer f.Close()
+
+	log.SetOutput(f)
+
 	ui.Run()
+}
+
+func truncateAndOpenLog() (*os.File, error) {
+	logPath := filepath.Join(os.TempDir(), "simdexplorer.log")
+
+	f, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+	if err != nil {
+		return nil, err
+	}
+	if err := f.Close(); err != nil {
+		return nil, err
+	}
+
+	return os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 }
